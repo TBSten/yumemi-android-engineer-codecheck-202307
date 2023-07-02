6d94192c167ba759e4e5819c50dc0da87398fdce
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-02 13:22:18 +0900

    RepositoryListを追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/search/RepositorySearchScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/search/RepositorySearchScreen.kt
@@ -1,7 +1,10 @@
 package jp.co.yumemi.android.code_check.repository.search
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
@@ -12,31 +15,71 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import jp.co.yumemi.android.code_check.R
+import jp.co.yumemi.android.code_check.repository.GithubRepository
+import jp.co.yumemi.android.code_check.repository.exampleGithubRepository
+import jp.co.yumemi.android.code_check.theme.CodeCheckTheme
 
 @Composable
 fun RepositorySearchScreen() {
-    RepositorySearchContent()
+    // 検証用
+    var text by remember { mutableStateOf("") }
+    val repositories = remember { List(10) { exampleGithubRepository } }
+
+    RepositorySearchContent(
+        searchQuery = text,
+        onChangeSearchQuery = { text = it },
+        repositories = repositories,
+        onClickRepository = { /* TODO goto detail page */ },
+    )
 }
 
 @Composable
-private fun RepositorySearchContent() {
+private fun RepositorySearchContent(
+    searchQuery: String,
+    onChangeSearchQuery: (String) -> Unit,
+    repositories: List<GithubRepository>?,
+    onClickRepository: (GithubRepository) -> Unit,
+) {
     Scaffold(
         topBar = { RepositorySearchTopBar() },
     ) {
-        var text by remember { mutableStateOf("") }
-        Column(Modifier.padding(it)) {
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
             Box(Modifier.padding(16.dp).fillMaxWidth()) {
                 SearchBar(
-                    value = text,
-                    onValueChange = { text = it },
+                    value = searchQuery,
+                    onValueChange = onChangeSearchQuery,
                 )
             }
-            // TODO:RepositoryList
+            if (repositories.isNullOrEmpty()) {
+                Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(
+                        if (repositories == null)
+                            "レポジトリを検索してね！"
+                        else
+                            "レポジトリがありません..."
+                    )
+                }
+            } else {
+                RepositoryList(
+                    repositories = repositories,
+                    onClickRepository = onClickRepository,
+                )
+            }
         }
+
     }
 }
 
@@ -45,4 +88,35 @@ private fun RepositorySearchTopBar() {
     TopAppBar(
         title = { Text(stringResource(R.string.app_name)) },
     )
+}
+
+
+@Preview(name = "dark", uiMode = UI_MODE_NIGHT_YES)
+@Preview(name = "light")
+annotation class LightDarkPreviews
+
+@LightDarkPreviews
+@Composable
+fun RepositorySearchContentPreview(
+    @PreviewParameter(PreviewGithubRepositoryProvider::class) repositories: List<GithubRepository>?,
+) {
+    CodeCheckTheme {
+        RepositorySearchContent(
+            searchQuery = "kotlin",
+            onChangeSearchQuery = {},
+            repositories = repositories,
+            onClickRepository = {},
+        )
+        Text("${isSystemInDarkTheme()}")
+    }
+}
+
+private class PreviewGithubRepositoryProvider : PreviewParameterProvider<List<GithubRepository>?> {
+    override val values
+        get() = sequenceOf(
+            null,
+            listOf(),
+            listOf(exampleGithubRepository),
+            List(100) { exampleGithubRepository },
+        )
 }
