7860795eb9d1fe19c2ee73ad6b77f459479e4194
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-03 23:21:51 +0900

    githubレポジトリのデータロジックをレポジトリに集約

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/search/RepositorySearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/search/RepositorySearchViewModel.kt
@@ -1,22 +1,27 @@
 package jp.co.yumemi.android.code_check.repository.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.co.yumemi.android.code_check.github.model.GithubRepoData
-import jp.co.yumemi.android.code_check.github.model.exampleGithubRepoData
+import jp.co.yumemi.android.code_check.github.model.GithubRepoDataRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RepositorySearchViewModel @Inject constructor() : ViewModel() {
+class RepositorySearchViewModel @Inject constructor(
+    private val repoDataRepository: GithubRepoDataRepository,
+) : ViewModel() {
     private val _searchQuery = MutableStateFlow("")
     val searchQuery = _searchQuery.asStateFlow()
 
     private val _repositoriesUiState = MutableStateFlow(
         RepositoriesUiState(
-            repositories = List(10) { exampleGithubRepoData },
+            isSearching = false,
+            repositories = emptyList(),
         )
     )
     val repositoriesUiState = _repositoriesUiState.asStateFlow()
@@ -24,8 +29,24 @@ class RepositorySearchViewModel @Inject constructor() : ViewModel() {
     fun updateSearchQuery(searchQuery: String) {
         _searchQuery.update { searchQuery }
     }
+
+    fun searchRepository() {
+        _repositoriesUiState.update { it.copy(isSearching = true) }
+        viewModelScope.launch {
+            val repositories = repoDataRepository.searchRepositories(searchQuery.value)
+            _repositoriesUiState.update {
+                it.copy(
+                    isSearching = false,
+                    repositories = repositories,
+                )
+            }
+        }.invokeOnCompletion {
+            _repositoriesUiState.update { it.copy(isSearching = false) }
+        }
+    }
 }
 
 data class RepositoriesUiState(
+    val isSearching: Boolean,
     val repositories: List<GithubRepoData>,
 )
