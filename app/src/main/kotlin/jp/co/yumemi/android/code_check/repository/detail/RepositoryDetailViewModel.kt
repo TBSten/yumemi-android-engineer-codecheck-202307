c2e8490ca45b2b674a707bb3767f63acabf446fe
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-05 22:33:50 +0900

    詳細画面の状態にローディングとエラーを追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/detail/RepositoryDetailViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/detail/RepositoryDetailViewModel.kt
@@ -1,11 +1,11 @@
 package jp.co.yumemi.android.code_check.repository.detail
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.co.yumemi.android.code_check.github.model.GithubRepoData
 import jp.co.yumemi.android.code_check.github.model.GithubRepoDataRepository
-import jp.co.yumemi.android.code_check.github.model.exampleGithubRepoData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -17,18 +17,22 @@ class RepositoryDetailViewModel @Inject constructor(
     private val repoDataRepository: GithubRepoDataRepository,
 ) : ViewModel() {
     private val _repositoryUiState = MutableStateFlow(
-        RepositoryUiState(
-            repository = exampleGithubRepoData
-        )
+        RepositoryUiState.init
     )
     val repositoryUiState = _repositoryUiState.asStateFlow()
     fun initRepository(repoName: String) {
         viewModelScope.launch {
-            val repository = repoDataRepository.getRepoData(repoName)
-            _repositoryUiState.update {
-                it.copy(
-                    repository = repository,
-                )
+            try {
+                _repositoryUiState.update { it.toLoading() }
+                val repository = repoDataRepository.getRepoData(repoName)
+                if (repository == null) {
+                    _repositoryUiState.update { it.toError() }
+                    return@launch
+                }
+                _repositoryUiState.update { it.toSuccess(repository) }
+            } catch (e: Exception) {
+                Log.e("error", "init repository", e)
+                _repositoryUiState.update { it.toError() }
             }
         }
     }
@@ -36,4 +40,30 @@ class RepositoryDetailViewModel @Inject constructor(
 
 data class RepositoryUiState(
     val repository: GithubRepoData?,
-)
+    val isLoading: Boolean,
+    val isError: Boolean,
+) {
+    companion object {
+        val init = RepositoryUiState(
+            repository = null,
+            isLoading = true,
+            isError = false,
+        )
+    }
+
+    fun toLoading() = this.copy(
+        isLoading = true,
+        isError = false,
+    )
+
+    fun toSuccess(repository: GithubRepoData) = this.copy(
+        repository = repository,
+        isLoading = false,
+        isError = false,
+    )
+
+    fun toError() = this.copy(
+        isLoading = false,
+        isError = true,
+    )
+}
