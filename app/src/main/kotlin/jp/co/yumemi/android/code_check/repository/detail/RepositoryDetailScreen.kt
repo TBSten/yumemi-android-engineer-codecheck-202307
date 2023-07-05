e1e5f51d02c0d5d833623aba473ac26d4c6fee12
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-05 22:18:42 +0900

    詳細画面でスクロールするように修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/detail/RepositoryDetailScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/detail/RepositoryDetailScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -59,7 +61,8 @@ fun RepositoryDetailScreen(
 fun RepositoryDetailContent(
     repository: GithubRepoData,
 ) {
-    Column(Modifier.padding(horizontal = 8.dp)) {
+    val scrollState = rememberScrollState()
+    Column(Modifier.verticalScroll(scrollState).padding(horizontal = 8.dp)) {
         val imageModifier =
             Modifier
                 .padding(8.dp)
