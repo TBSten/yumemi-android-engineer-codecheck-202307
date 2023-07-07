3318fc031b5ccf7ffa2b870141525d9ee0c7999d
TBSten <81161390+TBSten@users.noreply.github.com>
2023-07-08 02:11:59 +0900

    RepositoryListの各要素の実装をListItemに置き換え

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/search/RepositoryList.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/search/RepositoryList.kt
@@ -1,17 +1,13 @@
 package jp.co.yumemi.android.code_check.repository.search
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import jp.co.yumemi.android.code_check.components.ListItem
+import jp.co.yumemi.android.code_check.components.ListItemTailNext
 import jp.co.yumemi.android.code_check.github.model.GithubRepoData
 
 @Composable
@@ -21,15 +17,11 @@ fun RepositoryList(
 ) {
     LazyColumn(Modifier.fillMaxSize()) {
         items(repositories) { repository ->
-            Column(Modifier.clickable { onClickRepository(repository) }) {
-                Row(
-                    Modifier
-                        .padding(8.dp)
-                ) {
-                    Text(repository.name)
-                }
-                Divider()
-            }
+            ListItem(
+                onClick = { onClickRepository(repository) },
+                content = { Text(repository.name) },
+                tail = { ListItemTailNext() },
+            )
         }
     }
 }
